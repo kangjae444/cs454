b4a03727fbbbf77589145abfb73989e2babde6d6
Eunseob Kim <esk223@kaist.ac.kr>
2019-12-03 17:16:00 +0900

    handle strings & clean up codes

--- get_features.py
+++ get_features.py
@@ -1,59 +1,77 @@
 import os
+import re
 
 
 class FeatureMaker(object):
     def __init__(self, path_dir="./test_cases/"):
-        self.path_dir = path_dir
-        self.test_cases_list = os.listdir(path_dir)
-        self.terms = []
+        self._path_dir = path_dir
+        self._test_cases_list = os.listdir(path_dir)
+        self.term_list = []
+        self._find_all_terms()
 
-    def find_all_terms(self):
-        for test_case in self.test_cases_list:
-            test_file = open(self.path_dir + test_case, 'r')
-            source_code = test_file.readlines()
-            source_code = self._clean_up_code(source_code)
-            print("// " + test_case)
-            for line in source_code:
-                print(line)
-            print()
-            test_file.close()
+    def term_num(self):
+        return len(self.term_list)
 
-    def _clean_up_code(self, source_code):
-        clean_code = []
-        comment_flag = False
-        # string_flag = False
+    def _find_all_terms(self):
+        replaced_letters_list = ['(', ')', '{', '}', '<', '>', '[', ']', '+', '-', '*', '/', '%', '=',
+                                 ',', '!', '&', '|', ';', '.', '\\"']
 
-        for line in source_code:
-            if comment_flag:
-                if "*/" in line:
-                    line = line.split("*/")[1]
-                    comment_flag = False
-                else:
+        def _clean_up_code(dirty_code):
+            clean_code = []
+            comment_flag = False
+
+            for line in dirty_code:
+                if comment_flag:
+                    if "*/" in line:
+                        line = line.split("*/")[1]
+                        comment_flag = False
+                    else:
+                        continue
+                if "#include" in line:
                     continue
-            if "#include" in line:
-                continue
-            if "#define" in line:
-                line = line.split("#define")[1]
-            if "//" in line:
-                line = line.split("//")[0]
-            if "/*" in line:
-                if "*/" in line:
-                    first_part = line.split("/*")[0]
-                    second_part = line.split("*/")[1]
-                    line = first_part + second_part
-                else:
-                    line = line.split("/*")[0]
-                    comment_flag = True
-            line = line.replace('(', ' ').replace('{', ' ').replace('[', ' ')
-            line = line.replace(')', ' ').replace('}', ' ').replace(']', ' ')
-            line = line.replace('+', ' ').replace('-', ' ').replace(',', ' ').replace('!', ' ').replace(';', ' ')
-            line = line.replace('=', ' ').replace('>', ' ').replace('<', ' ').replace('*', ' ').replace('&', ' ')
-            line = line.replace('\\"', ' ')
-            line = line.strip()
+                if "#define" in line:
+                    line = line.split("#define")[1]
+                if "//" in line:
+                    line = line.split("//")[0]
+                if "/*" in line:
+                    if "*/" in line:
+                        first_part = line.split("/*")[0]
+                        second_part = line.split("*/")[1]
+                        line = first_part + second_part
+                    else:
+                        line = line.split("/*")[0]
+                        comment_flag = True
 
-            if line != '':
-                clean_code.append(line)
-        return clean_code
+                for replaced_letter in replaced_letters_list:
+                    line = line.replace(replaced_letter, ' ')
+                if '"' in line:
+                    string_list = re.findall('"([^"]*)"', line)
+                    for string in string_list:
+                        line = line.replace('"{0}"'.format(string), ' SOME_STRING ')
+                line = line.strip()
+
+                if line != '':
+                    clean_code.append(line)
+            return clean_code
+
+        for test_case in self._test_cases_list:
+            test_file = open(self._path_dir + test_case, 'r')
+            source_code = test_file.readlines()
+            source_code = _clean_up_code(source_code)
+            for clean_line in source_code:
+                term_list = clean_line.split()
+                for term in term_list:
+                    if self._is_new_term(term):
+                        self.term_list.append(term)
+            test_file.close()
+        self.term_list.sort()
+
+    def _is_new_term(self, term):
+        if term in self.term_list:
+            return False
+        if term.isdigit() or term[:2] == "0x":
+            return False
+        return True
 
 
 class TestCaseInfo(object):
@@ -61,5 +79,6 @@ class TestCaseInfo(object):
         self.file_name = file_name
         self.features = []
 
+
 fm = FeatureMaker()
-fm.find_all_terms()
+print(fm.term_list)
